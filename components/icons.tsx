97154020c9433a87359125c5f0c7d2b269f0b76a
omega-cop <omegagreen4@gmail.com>
2025-11-28 09:42:33 +0700

    Update icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -137,6 +137,12 @@ export const SwatchIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     </svg>
 );
 
+export const Bars2Icon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+    </svg>
+);
+
 export const GoldMedalIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg" {...props}>
     <defs>
