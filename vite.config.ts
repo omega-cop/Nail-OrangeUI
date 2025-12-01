5d1ec9fe98531791adf794972537c4dc65df4afb
omega-cop <omegagreen4@gmail.com>
2025-12-01 13:42:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -6,7 +7,7 @@ import path from 'path';
 export default defineConfig(({ mode }) => {
     // Load env file based on `mode` in the current working directory.
     // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
-    const env = loadEnv(mode, process.cwd(), '');
+    const env = loadEnv(mode, (process as any).cwd(), '');
     
     return {
       server: {
@@ -60,7 +61,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve(process.cwd()),
+          '@': path.resolve((process as any).cwd()),
         }
       }
     };
